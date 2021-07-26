02f7feb4fe9806ef85a9a64762bbcd84591c0bd2
Troy Dorman <troydorman@gmail.com>
2021-07-26 07:44:45 -0600

    installed dotenv and added our api key to a local-only .env file

--- bin/coindex.js
+++ bin/coindex.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 const program = require("commander");
 const pkg = require("../package.json");
+require("dotenv").config();
+
+console.log("api key " + process.env.NOMICS_API_KEY);
 
 program
   .version(pkg.version)
   .command("key", "Manage API Key -- https://nomics.com")
   .parse(process.argv);
-
-
